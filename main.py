cb576aa22f6bb185734a7545e0958d8cb63bade4
NYANYUIE Michael <mnyanyuie@gmail.com>
2024-04-18 23:21:21 +0200

    started with landing screen

--- main.py
+++ main.py
@@ -5,17 +5,28 @@ root.title('FeastFriend')
 root.geometry('500x500')
 root.config(bg='#F2EFE4')
 
-welcome_mes = Label(root, text="FeastFriends", font=('monospace', 10, 'bold'), fg= "#8C6954", bg="#F2EFE4")
+welcome_mes = Label(root, text="FeastFriends", font=('monospace', 10, 'bold'), fg= "#7E5A3C", bg="#F2EFE4")
 welcome_mes.grid(row=0, column=0)
 
-events_frame = Frame(root, bg="#BD5942")
-btn_frame = Frame(root, bg='#BD5942')
+events_wrap = Frame(root, bg="#BD5942", borderwidth=2, highlightcolor="#E4C39E", highlightthickness=2)
+events_wrap.grid(row=1, column=0)
+
+events_lab = Label(events_wrap, text="REGISTERED EVENTS", font=('monospace', 10, 'bold'), fg= "#FFF6ED", bg="#FF7028")
+events_lab.grid(row=0, column=0)
+
+events_frame = Frame(events_wrap, bg="#FFFDF3")
+events_frame.grid(row=1, column=0)
+
+event_lab = Label(events_frame, text="REGISTERED EVENTS", font=('monospace', 10, 'bold'), fg= "#A14D1B", bg="#FFFDF3")
+event_lab.grid(row=0, column=0)
+
+btn_frame = Frame(root, bg='#FFDCB6')
 btn_frame.grid(row=2, column=0)
 
-add_event_btn = Button(btn_frame, text="ADD EVENT", font=('monospace', 13, 'bold'), bg= "#009178", fg="#A1AF9F", activebackground='#DA9D82', activeforeground='#F2EFE4')
+add_event_btn = Button(btn_frame, text="ADD EVENT", font=('monospace', 13, 'bold'), bg= "#17D173", fg="#D9EDDF", activebackground='#DA9D82', activeforeground='#F2EFE4')
 add_event_btn.grid(row=0, column=0, padx=5, pady=5)
 
-del_event_btn = Button(btn_frame, text="DELETE EVENT", font=('monospace', 13, 'bold'), bg= "#912900", fg="#B0A0AB", activebackground='#DA9D82', activeforeground='#C4515C')
+del_event_btn = Button(btn_frame, text="DELETE EVENT", font=('monospace', 13, 'bold'), bg= "#523B32", fg="#FFF5EE", activebackground='#DA9D82', activeforeground='#C4515C')
 del_event_btn.grid(row=0, column=1, padx=(0,5))
 
 
